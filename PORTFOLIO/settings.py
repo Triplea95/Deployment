0c36c0d6b47e4b4d20434d6edbbec130ac852265
Triplea95 <odumosuadijat95@gmail.com>
2025-05-10 18:07:32 +0300

    firstcommit

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deployment-fh6r.onrender.com']
 
 
 # Application definition
